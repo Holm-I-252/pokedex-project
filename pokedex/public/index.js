4cd55667f4667da984a38dfbc14949679816cc43
Ian Holm <ianholm252@gmail.com>
2021-09-01 15:59:29 -0600

    first upload

---
+++ pokedex/public/index.js
@@ -0,0 +1,10 @@
+// document.getElementById("enterPoke").addEventListener("submit", () => {
+//   let name = document.getElementById("pokeInput").value;
+//   axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then((res) => {
+//     console.log(res.data);
+//     let pokeObj = {
+//       name,
+//       url,
+//     };
+//   });
+// });
---
+++ pokedex/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/style", express.static("./public/style.css"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+let port = process.env.PORT || 5252;
+
+app.listen(port, () => {
+  console.log(`running on ${port}`);
+});
